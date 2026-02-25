ad833231ee033eae77a4d33f0972abda9131523d
Gwyneth Salud <gcsalud@student.apc.edu.ph>
2026-02-25 19:02:59 +0800

    Updated backend logic

--- portfolio/backend/src/app.module.ts
+++ portfolio/backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GuildModule } from './guild/guild.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GuildModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+    GuildModule],
   controllers: [AppController],
   providers: [AppService],
 })
